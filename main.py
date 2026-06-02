fac6068d522bd84b731c6f8dafd3688fbfb1fa6d
Angel <1402482034@qq.com>
2026-06-02 23:13:40 +0800

    fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     parser.add_argument("--TASK_TYPE", type=str, default=Config.TASK_TYPE, choices=["binary", "multiclass"], help=f"任务类型 (默认: {Config.TASK_TYPE})")
     parser.add_argument("--BATCH_SIZE", type=int, default=Config.BATCH_SIZE, help=f"批大小 (默认: {Config.BATCH_SIZE})")
     parser.add_argument("--test_video", type=str, default="/share/data/公交检测/videos/000a385e2edd_02_65_6501_3_f0e4b440123b471592064da5c9cae9d8", help="测试视频路径 (默认: data/videos/000a385e2edd_02_65_6501_3_f0e4b440123b471592064da5c9cae9d8)")
-    parser.add_argument("RUN_MODE", type=str, default="train", choices=["train", "test", "predict"], help="运行模式: 'train' 训练, 'test' 评估, 'predict' 综合预测")
+    parser.add_argument("--RUN_MODE", type=str, default="train", choices=["train", "test", "predict"], help="运行模式: 'train' 训练, 'test' 评估, 'predict' 综合预测")
     args = parser.parse_args()
 
     # 用命令行参数覆盖 Config
